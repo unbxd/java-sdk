ce3726e6aeb98ec42fa6dd7d1625c3b5690007ed
Sourabh Agarwal <sourabh@unbxd.com>
2014-07-17 12:50:51 +0530

    basic test for autosuggest client

--- src/test/java/com/unbxd/client/AutoSuggestClientTest.java
+++ src/test/java/com/unbxd/client/AutoSuggestClientTest.java
@@ -20,19 +20,24 @@ public class AutoSuggestClientTest extends TestCase{
     protected void setUp() throws Exception {
         super.setUp();
 
-        Unbxd.configure("demo-u1393483043451", "ae30782589df23780a9d98502388555f", "ae30782589df23780a9d98502388555f");
+        Unbxd.configure("autosuggesttest-u1405357792247", "7db139ac885f6516fb276520668daf83", "7db139ac885f6516fb276520668daf83");
     }
 
     public void test_autosuggest() throws AutoSuggestException, ConfigException {
         AutoSuggestResponse response = Unbxd.getAutoSuggestClient()
                 .autosuggest("sh")
+                .setInFieldsCount(3)
+                .setKeywordSuggestionsCount(5)
+                .setPopularProductsCount(10)
+                .setTopQueriesCount(4)
                 .execute();
 
+        System.out.println(response.toString());
         Assert.assertNotNull(response);
-//        Assert.assertEquals(0, response.getStatusCode());
-//        Assert.assertNotEquals(0, response.getQueryTime());
-//        Assert.assertEquals(0, response.getErrorCode());
-//        Assert.assertEquals("OK", response.getMessage());
-//        Assert.assertNotEquals(0, response.getTotalResultsCount());
+        Assert.assertEquals(0, response.getStatusCode());
+        Assert.assertNotEquals(0, response.getQueryTime());
+        Assert.assertEquals(0, response.getErrorCode());
+        Assert.assertEquals("OK", response.getMessage());
+        Assert.assertNotEquals(0, response.getTotalResultsCount());
     }
 }
